6a2183b96a0a341d5ee2ddb7630bc1ba219ab492
YB <whyis2cool@gmail.com>
2019-06-17 21:12:37 -0500

    Created base class for items and 1 child sill not finish
    the base class is an abstract , the child is the lamp item

---
+++ LoZ A Link to the Past/Assets/Item Scripts/ItemBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//! used to know which type of item the player is using 
+public enum ItemType
+{
+  Unknown,
+  Collection,//<! means the item takes affect when collected example : hearts 
+  PlayerUse,//<! means the player can chose when to use the item.
+}
+
+public abstract class ItemBase : MonoBehaviour
+{
+  //! this is just for debugging
+  public string m_ItemName { get; set; }
+  //! this is used to know the type of the item 
+  public ItemType m_ItemType { get; set; }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    m_ItemName = "Base Item class";
+    Debug.Log(m_ItemName);
+  }
+  //! depending on the item this can represent direction OR position
+  public abstract void ItemAcction(Vector2 vector);
+}
---
+++ LoZ A Link to the Past/Assets/Item Scripts/ItemLamp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Item_Scripts
+{
+  /// <summary>
+  /// all this does is spawn a sprite and collision box in the direction thats front
+  /// </summary>
+  public class ItemLamp : ItemBase
+  {
+    // use this to render the fire from the lamp 
+    SpriteRenderer spriteRenderer;
+
+    public Sprite FireSprite;
+    public float m_fireTime;
+
+    private void Start()
+    {
+      spriteRenderer = gameObject.AddComponent< SpriteRenderer >() as SpriteRenderer;
+      m_ItemName = "Lamp";
+      m_ItemType = ItemType.PlayerUse;
+      // this is temporay REMOVE WHEN DONE  
+      UnityEngine.Vector2 Dir = new Vector2(-1, 0);
+      m_fireTime = 0.05f;
+
+      ItemAcction(Dir);
+
+      Debug.Log("Item Lamp");
+    }
+
+    public override void ItemAcction(Vector2 Direction)
+    {
+      Vector3 Temp = new Vector3();
+      Temp = spriteRenderer.transform.position;
+      Temp.x += Direction.x;
+      Temp.y += Direction.y;
+
+      Task Stun = Task.Factory.StartNew(() =>
+      {
+        while (m_HitStunTime > 0.0f)
+        {
+          m_HitStunTime -= Time.deltaTime;
+          Debug.Log("You are Stunned");
+        }
+        m_HitStunTime = m_HisStunAmount;
+      });
+
+
+      spriteRenderer.transform.position = Temp;
+    
+      spriteRenderer.sprite = FireSprite;
+
+      Debug.Log("Do something");
+    }
+  }
+}
