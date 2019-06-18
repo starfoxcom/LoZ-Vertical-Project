0bb66010d5b0994646e81cee9dad1fc989830050
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2019-06-18 13:01:11 -0500

    Create enemies prefabs and class files

---
+++ LoZ A Link to the Past/Assets/scripts/Enemies/Enemy_Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour
+{
+
+  /************************************************************************/
+  /* Public                                                               */
+  /************************************************************************/
+
+  /************************************************************************/
+  /* Private                                                              */
+  /************************************************************************/
+
+  // Start is called before the first frame update
+  void
+  Start()
+  {
+    // get components
+
+    // FSM
+
+    m_fsm = new FSM();
+
+    m_fsm.AddState(new Enemy_Wander());
+
+    m_fsm.SetState(ENEMY_GLOBALS.WANDER_STATE_ID);
+
+    return;
+  }
+
+  // Update is called once per frame
+  void
+  Update()
+  {
+
+  }
+
+  private FSM m_fsm;
+}
