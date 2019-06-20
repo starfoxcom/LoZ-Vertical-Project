838a908ff30703c5498f0a98b9d75a88d62cdc42
YB <whyis2cool@gmail.com>
2019-06-20 08:16:01 -0500

    Create torch classes

---
+++ LoZ A Link to the Past/Assets/scripts/Item Scripts/Dynamic/TorchOffState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.scripts.Item_Scripts.Dynamic
+{
+  // for detecting collisions
+  class TorchOffState : BaseDynamicItem
+  {
+    public bool IsTorchOff = true;
+
+    public override bool DynamicAcction()
+    {
+      if (!IsTorchOff)
+      {
+        IsTorchOff = false;
+        return IsTorchOff;
+      }
+
+      return true;
+    }
+
+    public bool CheckTorch()
+    {
+      return IsTorchOff;
+    }
+
+
+  }
+}
