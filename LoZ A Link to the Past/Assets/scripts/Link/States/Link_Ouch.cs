c488ae2bc8512d765f694b37cd1c6dcfb7fe79f1
MaxMald <nuup20@gmail.com>
2019-06-22 21:34:51 -0500

    feat: Link can recibe damage

---
+++ LoZ A Link to the Past/Assets/scripts/Link/States/Link_Ouch.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Link_Ouch
+  : State
+{
+
+  public Link_Ouch(GameObject _gameObject, FSM _fsm)
+  {
+    m_id = LINK_GLOBALS.OUCH_STATE_ID;
+
+    m_fsm = _fsm;
+    m_gameObject = _gameObject;
+
+    m_link_cntrl =      m_gameObject.GetComponent<Link_Controller>();
+    m_link_animation =  m_gameObject.GetComponent<Link_Animation_Controller>();
+    m_link_move =       m_gameObject.GetComponent<Link_Movement>();    
+
+    return;
+  }
+
+  public override void
+  OnExit()
+  {
+    m_link_animation.DesactiveHit();
+    m_link_cntrl.ActiveInmune();
+    return;
+  }
+
+  public override void
+  OnPrepare()
+  {
+    Vector2 enemy_position = m_link_cntrl.GetEnemeyPosition();
+
+    m_hit_direction = m_gameObject.transform.position;
+    m_hit_direction -= enemy_position;
+    m_hit_direction.Normalize();
+    m_link_animation.ActiveHit();
+
+    m_trigger_time = Time.time + m_hit_time;
+
+    return;
+  }
+
+  public override void
+  Update()
+  {
+    m_link_move.UpdateDisplacement(m_hit_direction);
+
+    if(Time.time >= m_trigger_time)
+    {
+      m_fsm.SetState(LINK_GLOBALS.IDLE_STATE_ID);
+      return;
+    }
+
+    return;
+  }
+
+  private float m_hit_time = 0.4f;
+
+  private float m_trigger_time;
+
+  private Vector2       m_hit_direction;
+
+  private Link_Movement m_link_move;
+
+  private Link_Animation_Controller m_link_animation;
+
+  private Link_Controller m_link_cntrl;
+}
+
